9e38bfd01c264197c9f915ded008710ad94f6a33
vahid <vahid91630@gmail.com>
2025-07-05 21:49:06 +0330

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,11 +1,6 @@
 import os
 from dotenv import load_dotenv
 
-# بارگذاری متغیرهای محیطی
 load_dotenv()
 
-# توکن ربات تلگرام
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-if not BOT_TOKEN:
-    raise ValueError("❌ BOT_TOKEN تعریف نشده! مطمئن شو فایل .env در روت پروژه هست.")
+BOT_TOKEN: str = os.getenv("BOT_TOKEN", "")
