ed417a5c0f80b2de6a9d7080972482951444a199
vahid <vahid91630@gmail.com>
2025-07-06 00:46:25 +0330

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,14 +1,8 @@
 from fastapi import FastAPI
-from bot.telegram import dp, bot
+from app.bot.telegram import dp, bot
 
 app = FastAPI()
 
-
 @app.on_event("startup")
-async def on_startup():
-    print("Bot starting...")
-
-
-@app.get("/")
-async def root():
-    return {"status": "OK"}
+async def startup():
+    await bot.set_webhook(os.getenv("WEBHOOK_URL"))
