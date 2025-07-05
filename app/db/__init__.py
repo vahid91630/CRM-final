3a379c85cb71ad01837a4630885c616a86eec3db
vahid <vahid91630@gmail.com>
2025-07-05 23:37:12 +0330

    Update __init__.py

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -1,9 +1,1 @@
-from .session import get_db, engine
-from .base_class import Base
-from .models import *  # اگر مدل‌ها در یک فایل مجزا هستن، این خط باید درست اشاره کنه
-
-__all__ = [
-    "get_db",
-    "engine",
-    "Base",
-]
+# Keeps db module initialized
