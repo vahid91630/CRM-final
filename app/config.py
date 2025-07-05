1d63e79d624e1e63bae9eb8715e28a24f5ff9ab9
vahid <vahid91630@gmail.com>
2025-07-05 23:27:24 +0330

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,10 +1,11 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str
-    BOT_TOKEN: str
-    WEBHOOK_URL: str
+    bot_token: str
+    webhook_url: str
+    database_url: str
 
-    model_config = SettingsConfigDict(env_file=".env")
+    class Config:
+        env_file = ".env"
 
 settings = Settings()
