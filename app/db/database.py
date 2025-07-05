7dacdab833f241eba1b276ff4986a0018725c3a1
vahid <vahid91630@gmail.com>
2025-07-05 19:52:27 +0330

    Update database.py

--- app/db/database.py
+++ app/db/database.py
@@ -1,8 +1,16 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./crm.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+load_dotenv()
 
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./crm.db")
+
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {}
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
