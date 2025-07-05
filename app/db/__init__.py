cfe3b72e731ab5a1111b55dba6a7c82d9cc013be
vahid <vahid91630@gmail.com>
2025-07-05 21:34:13 +0330

    Update __init__.py

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -1,2 +1,7 @@
-from .database import get_session
-from .models import Customer
+# db/__init__.py
+# این فایل برای شناسایی پوشه db به‌عنوان یک پکیج پایتون ایجاد شده است.
+# می‌توانید در صورت نیاز importهای پایه دیتابیس را اینجا قرار دهید.
+
+# مثال:
+# from .database import get_session
+# from .models import Customer
