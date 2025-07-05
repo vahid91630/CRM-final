ca393b5a930de6237bf0b3819d08b31e845850f9
vahid <vahid91630@gmail.com>
2025-07-05 23:12:51 +0330

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,11 +1,10 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
+    DATABASE_URL: str
     BOT_TOKEN: str
     WEBHOOK_URL: str
-    DATABASE_URL: str = "sqlite+aiosqlite:///./db.sqlite3"
 
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(env_file=".env")
 
 settings = Settings()
