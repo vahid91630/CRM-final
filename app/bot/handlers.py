34f73ef875f7e140d5a645604b1c17772306706b
vahid <vahid91630@gmail.com>
2025-07-06 15:45:42 +0330

    Delete app/bot/handlers.py

--- app/bot/handlers.py
+++
@@ -1,6 +0,0 @@
-from aiogram import types
-from app.bot.telegram import dp
-
-@dp.message_handler(commands=['start', 'سلام'])
-async def start_handler(message: types.Message):
-    await message.answer("سلام 👋 من دستیار CRM شما هستم. چطور می‌تونم کمکتون کنم؟")
