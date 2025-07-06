f574ae40cbeb75d118741af335c164f3b6875c7c
vahid <vahid91630@gmail.com>
2025-07-06 13:18:30 +0330

    Update customer.py

--- app/db/models/customer.py
+++ app/db/models/customer.py
@@ -1,10 +1,13 @@
-from sqlalchemy import Column, Integer, String
-from app.db.database import Base
+# app/models/customer.py
+
+from sqlalchemy import Column, Integer, String, DateTime, func
+from app.db.base import Base
 
 class Customer(Base):
     __tablename__ = "customers"
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
-    phone = Column(String, unique=True, index=True)
-    email = Column(String, nullable=True)
+    id: int = Column(Integer, primary_key=True, index=True)
+    full_name: str = Column(String(100), nullable=False)
+    phone_number: str = Column(String(20), unique=True, nullable=False)
+    email: str = Column(String(100), nullable=True)
+    created_at: DateTime = Column(DateTime(timezone=True), server_default=func.now())
