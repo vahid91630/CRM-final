2d2b52b79ca76a9c43048a77ca5e4d27bc93efea
vahid <vahid91630@gmail.com>
2025-07-06 12:45:01 +0330

    Update customer.py

--- app/db/models/customer.py
+++ app/db/models/customer.py
@@ -1,12 +1,10 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String
 from app.db.database import Base
-import datetime
 
 class Customer(Base):
     __tablename__ = "customers"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
-    phone = Column(String, unique=True, nullable=False)
+    phone = Column(String, unique=True, index=True)
     email = Column(String, nullable=True)
-    created_at = Column(DateTime, default=datetime.datetime.utcnow)
