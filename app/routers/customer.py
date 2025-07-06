a3b2a7df0fcb81aa866a5a04838b69aab019c5e8
vahid <vahid91630@gmail.com>
2025-07-06 12:25:46 +0330

    Update customer.py

--- app/routers/customer.py
+++ app/routers/customer.py
@@ -1,10 +1,20 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
-router = APIRouter(
-    prefix="/customers",
-    tags=["Customers"]
-)
+router = APIRouter(prefix="/customers", tags=["Customers"])
+
+class Customer(BaseModel):
+    name: str
+    phone: str
+    email: str = None
+
+fake_db = []
+
+@router.post("/")
+async def create_customer(customer: Customer):
+    fake_db.append(customer)
+    return {"msg": "Customer added", "customer": customer}
 
 @router.get("/")
 async def list_customers():
-    return {"customers": ["Alice", "Bob", "Charlie"]}
+    return fake_db
