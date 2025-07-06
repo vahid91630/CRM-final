5b4b1879a84996b0bb0ab61b4cdd84a10be165d9
vahid <vahid91630@gmail.com>
2025-07-06 15:34:54 +0330

    Update __init__.py

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -1,1 +1,9 @@
+from app.db.base import Base
+from app.database import engine
+import asyncio
 
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(
+if __name__ == "__main__":
+    asyncio.run(init_db())
