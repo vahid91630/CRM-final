c9d3e7ee5e285928a58e268c8f8711154e552fc1
vahid <vahid91630@gmail.com>
2025-07-06 12:24:03 +0330

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,28 +1,10 @@
-import os
 from fastapi import FastAPI
-from dotenv import load_dotenv
-import sentry_sdk
-from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
-
 from app.routers import customer
 
-load_dotenv()
-
-sentry_sdk.init(
-    dsn=os.getenv("SENTRY_DSN"),
-    traces_sample_rate=1.0,
-    environment="production"
-)
-
 app = FastAPI()
-app = SentryAsgiMiddleware(app)
 
 app.include_router(customer.router)
 
 @app.get("/")
 async def root():
-    return {"message": "CRM running"}
-
-@app.get("/crash")
-async def crash():
-    raise Exception("Test Error")
+    return {"message": "CRM Backend is running"}
