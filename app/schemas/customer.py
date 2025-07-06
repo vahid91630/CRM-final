ec2f9345df9f3fdf2439faf349f8e2983f63e415
vahid <vahid91630@gmail.com>
2025-07-06 13:24:05 +0330

    Update customer.py

--- app/schemas/customer.py
+++ app/schemas/customer.py
@@ -1,16 +1,18 @@
 from pydantic import BaseModel
 from typing import Optional
+from datetime import datetime
 
 class CustomerBase(BaseModel):
-    name: Optional[str]
-    phone: Optional[str]
-    telegram_id: int
+    full_name: str
+    phone_number: str
+    email: Optional[str] = None
 
 class CustomerCreate(CustomerBase):
     pass
 
-class CustomerOut(CustomerBase):
+class CustomerRead(CustomerBase):
     id: int
+    created_at: datetime
 
     class Config:
         orm_mode = True
