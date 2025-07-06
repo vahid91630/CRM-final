62f5104242ae70fcffbf6bdcb8e05a127076fccd
vahid <vahid91630@gmail.com>
2025-07-06 13:45:41 +0330

    Update customer.py

--- app/schemas/customer.py
+++ app/schemas/customer.py
@@ -1,14 +1,13 @@
 from pydantic import BaseModel
 
 class CustomerBase(BaseModel):
-    full_name: str
-    phone_number: str
+    name: str
     email: str
 
 class CustomerCreate(CustomerBase):
     pass
 
-class CustomerRead(CustomerBase):
+class Customer(CustomerBase):
     id: int
 
     class Config:
