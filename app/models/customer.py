20360b1649f84cca699556aacd69cf46c1da1f5c
vahid <vahid91630@gmail.com>
2025-07-07 11:47:54 +0330

    Update customer.py

--- app/models/customer.py
+++ app/models/customer.py
@@ -1,10 +1,9 @@
 from sqlalchemy import Column, Integer, String
-from app.db.base import Base
+from app.database.base import Base
 
 class Customer(Base):
     __tablename__ = "customers"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=True)
-    phone = Column(String, nullable=True)
-    telegram_id = Column(Integer, unique=True, index=True, nullable=False)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
