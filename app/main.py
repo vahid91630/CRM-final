4a309337a7fdd3ec27c1d079decc1c58891956c7
vahid <vahid91630@gmail.com>
2025-07-07 11:44:25 +0330

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,16 +1,20 @@
 import uvicorn
 from fastapi import FastAPI
+
 from app.routers.customer_router import router as customer_router
 from app.database.base import Base
 from app.database.session import engine
 
-app = FastAPI()
+app = FastAPI(title="CRM API")
+
+# Register routes
 app.include_router(customer_router)
 
+# Auto create tables on startup
 @app.on_event("startup")
 async def on_startup():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000)
